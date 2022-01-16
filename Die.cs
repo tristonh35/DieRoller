90dcd1830396981d1768bf9fa2743b8fda4f20c4
tristonh35 <tristonhall35@yahoo.com>
2022-01-15 23:28:25 -0800

    Created a FaceValue property with documentation

--- Die.cs
+++ Die.cs
@@ -11,8 +11,12 @@ namespace DieRoller
     /// Represents a single six sided die (1-6)
     /// </summary>
     public class Die
-    {
-        // Face up value (what was rolled by the user?) 
+    {   
+        /// <summary>
+        /// The current face up value of the die
+        /// </summary>
+        public byte FaceValue { get; set; }
+        
         // Is the die held? 
 
         // Roll (set a new random face up value) 
